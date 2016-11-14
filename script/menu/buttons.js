9e8339e97fff77f87dc0941f6df82ad14f311980
gr <gr>
2016-11-14 13:50:23 +0100

    show hide buttons

---
+++ script/menu/buttons.js
@@ -0,0 +1,13 @@
+/**
+ * Created by root on 13.11.16.
+ */
+
+
+$(document).ready(function(){
+    $("#hidesl").click(function(){
+        $("#sliders").hide();
+    });
+    $("#showsl").click(function(){
+        $("#sliders").show();
+    });
+});
